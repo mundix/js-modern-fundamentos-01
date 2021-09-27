1c83423140fc1c0b12372603fc28dfcf41efa9cb
Edmundo Pichardo <ce.pichardo@gmail.com>
2021-09-27 03:48:32 -0400

    working with swith

---
+++ assets/js/switch.js
@@ -0,0 +1,20 @@
+
+
+// const dia = '2';
+const dia = 2;
+
+//El switch evalua === con trile igual evaluado el tipo de datos. 
+switch (dia) { 
+    case 0:
+        console.log('Domingo');
+        break;
+    case 1:
+        console.log('Lunes');
+        break;
+    case 2:
+        console.log('Martes');
+        break;
+        default:
+            console.log('No es lunes, martes o domingo');
+}
+
