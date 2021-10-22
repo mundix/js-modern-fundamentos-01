8c4369bb1f7c36ad4791c7c803329371b3fb5b60
Edmundo Pichardo <ce.pichardo@gmail.com>
2021-10-22 04:26:39 -0400

    Working with old class way sinse 2013 less

--- assets/js/classes/problema.js
+++ assets/js/classes/problema.js
@@ -1,1 +1,19 @@
 
+// Ok esto se debe de crear con la palabra new
+// Esta esla forma vieja que se usabahace 5 u 8 años 
+function Persona(nombre, edad) {
+    console.log('Se ejecuto esta lina');
+    this.nombre = nombre;
+    this.edad = edad;
+
+    this.imprimir = function() {
+        console.log(`Nombre: ${this.nombre} Edad: ${this.edad}`);
+    }
+
+}
+
+const maria = new Persona('Maria', 18);
+const melissa = new Persona('Melissa',35);
+
+maria.imprimir();
+melissa.imprimir();
