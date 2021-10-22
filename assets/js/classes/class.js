e46092e3368332d5f77a3a90b9381e798c3df7a5
Edmundo Pichardo <ce.pichardo@gmail.com>
2021-10-22 04:54:45 -0400

    How sets and gets works

--- assets/js/classes/class.js
+++ assets/js/classes/class.js
@@ -4,6 +4,7 @@ class Persona {
     // No se le ponen comma por que no es inicializaicon de variale , 
     // Toda las clases en javasscript viene de modo obligacorio como 'use strict'
     // Modo estricto de javascript 
+    // PROPERTIES OR VARIABLES OF THE CLASS
     nombre = '';
     codigo = '';
     frase = '';
@@ -11,12 +12,29 @@ class Persona {
 
     // El consteructor siempre devuevle una instancia del objeto , y nuca undefined 
     // El unico metodo que hace esto
+    // CONSTRUCTOR
     constructor(nombre = 'Sin nombre', codigo = 'Sin codido', frase = 'Sin Frase') {
 
         this.nombre = nombre;
         this.codigo = codigo;
         this.frase = frase;
     }
+
+    // SET y GET 
+    // Los set se les pone set en ves de function 
+    // El set no puede tener le mismo nombre de la propiedad a la cual queremos referirnos 
+    set setComidaFavorita(comida) {
+        this.comida = comida.toUpperCase();
+    }
+
+    get getComidaFavorita() {
+        return `La comida favorita es ${this.comida}`;
+        // Por eso no se puede llamar igual , para que no entre en loop infinitio
+        // return `La comida favorita es ${this.getComidaFavorita}`;
+    }
+
+
+    // METHODS 
 
     quienSoy() {
         console.log(`Soy ${this.nombre} y mi identidad es ${this.codigo}`);
@@ -37,10 +55,17 @@ class Persona {
 const spiderman = new Persona('Peter Parker', 'Spider', 'Soy tu amigable vecino Spiderman');
 const ironMan = new Persona('Tony Stark', 'Ironman', 'Yo soy ironman');
 
-console.log(spiderman);
-console.log(ironMan);
+// console.log(ironMan);
 
 // spiderman.quienSoy();
 spiderman.miFrase();
+// cuadno se llama el set , se ve como un cubito azul que indica que no es un metodos, 
+// Es como una propiedad para asignar
+// Y ahora sale dentro del prototype 
+spiderman.setComidaFavorita = 'Pie de la tia may';
+// spiderman.namesis = 'Duende Verde'; //Le asigna esta propiedad sin haberse declarado 
+console.log(spiderman.getComidaFavorita);
+console.log(spiderman);
+
 // ironMan.quienSoy();
-ironMan.miFrase();
+// ironMan.miFrase();
